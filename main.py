c26a1d223ac7283c5ba8129a8fd3fcdefbd2482a
JsMelix <juansmelo03@gmail.com>
2025-02-04 22:38:25 -0500

    update changes

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from exa_py import Exa
 
 exa = Exa('7dd52fee-c155-4ff1-9427-8ca7e949a6a1')
+
+query = input('Search here: ')
+
+response = exa.search(
+  query,
+  num_results=5,
+  type='keyword',
+  include_domains=['https://www.tiktok.com'],
+)
+
+print(response)
